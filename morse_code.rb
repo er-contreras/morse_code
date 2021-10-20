be75657b0c689413f5b77186f41a7e16a99b070c
Erick Contreras <eri89--@hotmail.com>
2021-10-20 16:23:23 -0500

    Decode a Morse code

---
+++ morse_code.rb
@@ -0,0 +1,17 @@
+MORSE_CODE = Hash[*%w[
+  A .- B -... C -.-. D -.. E . F ..-.
+  G --. H .... I .. J .--- K -.- L .-..
+  M -- N -. O --- P .--. Q --.- R .-.
+  S ... T - U ..- V ...- W .-- X -..-
+  Y -.-- Z --.. 1 .---- 2 ..--- 3 ...-- 4 ....-
+  5 ..... 6 -.... 7 --... 8 ---.. 9 ----. 0 -----
+]].invert.freeze
+
+def decode(str)
+  words = str.to_s.split(/\s{2,}/)
+  words.map { |w| w.split.map { |mc| MORSE_CODE[mc] }.join }.join ' '
+end
+
+puts decode('.-')
+puts decode('-- -.--   -. .- -- .')
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
